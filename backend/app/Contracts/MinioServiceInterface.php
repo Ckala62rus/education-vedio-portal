94bc1daa32c4aa6b95d95dd608cb0c3362621589
Ckala62rus <agr.akyla@mail.ru>
2024-11-30 21:42:20 +0300

    ( refactor ) some change MinioService.php and interface.

--- backend/app/Contracts/MinioServiceInterface.php
+++ backend/app/Contracts/MinioServiceInterface.php
@@ -7,6 +7,7 @@ use Illuminate\Http\UploadedFile;
 interface MinioServiceInterface
 {
     public function saveFile(UploadedFile $file): string;
-    public function getFileUrl(string $filename, string $folder): string;
+    public function getFileUrl(string $filename, string $folder): string|null;
     public function deleteFile(string $filename, string $folder): bool;
+    public function getAllFiles(string $path): array|null;
 }
