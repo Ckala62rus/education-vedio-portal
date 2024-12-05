88d0ddae8680348ff0591361f11820032a5f658f
Ckala62rus <agr.akyla@mail.ru>
2024-12-05 20:50:25 +0300

    ( feature ) added video player.

---
+++ backend/resources/js/Components/VideoPlayer.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="row">
+<!--        <div class="col-6 mx-auto">-->
+            <video ref="videoPlayer" class="video-js vjs-default-skin"></video>
+<!--        </div>-->
+    </div>
+
+</template>
+
+<script>
+import videojs from 'video.js';
+
+export default {
+    name: 'VideoPlayer',
+    props: {
+        options: {
+            type: Object,
+            default() {
+                return {};
+            }
+        }
+    },
+    data() {
+        return {
+            player: null
+        }
+    },
+    mounted() {
+        this.player = videojs(this.$refs.videoPlayer, this.options, () => {
+            this.player.log('onPlayerReady', this);
+        });
+    },
+    beforeDestroy() {
+        if (this.player) {
+            this.player.dispose();
+        }
+    }
+}
+</script>
